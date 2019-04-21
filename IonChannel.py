58bef4bbb6c937df986417d940296c6f7171353e
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2019-04-21 19:39:22 +0200

    Refactor imports

--- IonChannel.py
+++ IonChannel.py
@@ -1,7 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.stats as ss
-import seaborn as sns
+from numpy.random import random
+from matplotlib import pyplot
+from scipy import stats
+import seaborn
 
 OPEN = 1
 CLOSED = 0
@@ -24,7 +24,7 @@ def main():
     # measure for 10 minutes
     for x in range(1, 10*60*1000):
         # random number in half-open interval [0.0, 1.0)
-        rnd = np.random.random()
+        rnd = random()
         time_in_state += dt
         if rnd < (k * dt):
             log_interval(state, round(time_in_state, 3))
@@ -32,13 +32,13 @@ def main():
             time_in_state = 0
 
     # creating histogram and fit
-    sns.set_style('darkgrid')
-    sns.distplot(OPEN_TIMES,bins=100,fit=ss.expon,kde=False)
-    print(ss.expon.fit(OPEN_TIMES))
+    seaborn.set_style('darkgrid')
+    seaborn.distplot(OPEN_TIMES,bins=100,fit=stats.expon,kde=False)
+    print(stats.expon.fit(OPEN_TIMES))
 
-    plt.title("Histogram and exponentional fit of intervals open state")
-    plt.xlabel("Duration of open state (s)")
-    plt.ylabel("Occurunces")
+    pyplot.title("Histogram and exponentional fit of intervals open state")
+    pyplot.xlabel("Duration of open state (s)")
+    pyplot.ylabel("Occurunces")
 
     plt.show()
 
